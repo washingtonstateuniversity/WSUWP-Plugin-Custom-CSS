3f3c02fc00988dfc60072b13356dc2be02902386
Jeremy Felt <jeremy.felt@gmail.com>
2016-10-18 10:26:20 -0700

    Bump version 2.2.0

--- custom-css.php
+++ custom-css.php
@@ -4,7 +4,7 @@ Plugin Name: WSU Custom CSS
 Plugin URI: https://web.wsu.edu/
 Description: Custom CSS via custom post type.
 Author: washingtonstateuniversity, jeremyfelt, automattic
-Version: 2.1.6
+Version: 2.2.0
 */
 
 /**
